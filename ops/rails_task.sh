dd9e6c5d1e2e125de88ecfb32266f90b3f5d23d3
yokohama <yuhei.yokohama@gmail.com>
2022-10-14 22:58:23 +0900

    deploy test

--- ops/rails_task.sh
+++ ops/rails_task.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-AWS_PROFILE=secbokapp-cdk
+if [ "${GITHUB_ACTIONS}" = "true" ]; then
+  AWS_PROFILE=''
+else
+  AWS_PROFILE='--profile secbokapp-cdk'
+fi
 
 if [ "${TARGET_ENV}" = "" ]; then
   echo '[Error]'
@@ -21,14 +25,14 @@ OVERRIDE_FILE=./overrides.json
 
 FAMILY_NAME=task-${TARGET_ENV}
 
-CLUSTER_ARN=`aws ecs list-clusters --profile ${AWS_PROFILE} | jq -r '.clusterArns[]' | grep ${TARGET_ENV}`
+CLUSTER_ARN=`aws ecs list-clusters ${AWS_PROFILE} | jq -r '.clusterArns[]' | grep ${TARGET_ENV}`
 if [ "${CLUSTER_ARN}" = "" ]; then
   echo '[Error]'
   echo '- Cluster dose not exist'
   exit 1
 fi
 
-SERVICE_ARN=`aws ecs list-services --cluster ${CLUSTER_ARN} --profile ${AWS_PROFILE} | jq -r '.serviceArns[0]'`
+SERVICE_ARN=`aws ecs list-services --cluster ${CLUSTER_ARN} ${AWS_PROFILE} | jq -r '.serviceArns[0]'`
 if [ "${SERVICE_ARN}" = "" ]; then
   echo '[Error]'
   echo '- Cluster dose not exist'
@@ -38,13 +42,13 @@ fi
 TASK_DEF_ARN=$(aws ecs list-task-definitions \
   --family-prefix "${FAMILY_NAME}" \
   --query "reverse(taskDefinitionArns)[0]" \
-  --profile ${AWS_PROFILE} \
+  ${AWS_PROFILE} \
   --output text) \
 
 CONTAINER_NAME=$(aws ecs describe-task-definition \
   --task-definition "${TASK_DEF_ARN}" \
   --query "taskDefinition.containerDefinitions[0].name" \
-  --profile ${AWS_PROFILE} \
+  ${AWS_PROFILE} \
   --output text)  \
 && cat <<EOF > overrides.json
   {
@@ -60,7 +64,7 @@ EOF
 NETWORK_CONFIG=$(aws ecs describe-services \
   --cluster ${CLUSTER_ARN} \
   --services ${SERVICE_ARN} \
-  --profile ${AWS_PROFILE} \
+  ${AWS_PROFILE} \
   | jq '.services[].deployments[0].networkConfiguration')
 
 aws ecs run-task \
@@ -68,7 +72,7 @@ aws ecs run-task \
 --task-definition "${TASK_DEF_ARN}" \
 --network-configuration "${NETWORK_CONFIG}" \
 --launch-type FARGATE \
---profile ${AWS_PROFILE} \
+${AWS_PROFILE} \
 --overrides file://overrides.json
 
 rm -rf ${OVERRIDE_FILE}
