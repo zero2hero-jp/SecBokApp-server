2830d02a1c069bc5cf1248f59ed6230c666a6c74
yuukii125 <y.kipro125@gmail.com>
2022-11-29 19:57:02 +0900

    factorybotのドメイン変更

--- spec/factories/sheets.rb
+++ spec/factories/sheets.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :sheet do
-    sequence(:email) { |n| "test#{n}@example.com" }
+    sequence(:email) { |n| "test#{n}@zero2hero.jp" }
     sequence(:spreadsheet_id) { |n| "#{n}" }
   end
 end
